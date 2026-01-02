5e472fdbfee0f28666571b448b2bd11d9978d792
OliviaZonnedda <olivia.zonnedda@student.ehb.be>
2026-01-02 13:46:54 +0100

    fixDatabase

--- back/app.js
+++ back/app.js
@@ -3,54 +3,43 @@ const app = express()
 const port = 3000
 
 const uri = "mongodb+srv://olivia:ipjXcBgjQrV0XfSh@cluster1.rle79lp.mongodb.net/?appName=Cluster1";
-
 const { MongoClient } = require("mongodb");
 
 
-
-
 const client = new MongoClient(uri);
-const dbName = "Cluster1"
-async function run() {
+const dbName = "cityBite"
+
+
+async function startServer() {
     try {
-        // Connect to the Atlas cluster
         await client.connect();
-        console.log("connected correctly toserver");
-        // Get the database and collection on which to run the operation
+        console.log("Connected to MongoDB");
+
         const db = client.db(dbName);
-        const col = db.collection("cityBite");
+        const col = db.collection("foodSpot");
 
-        // Create new documents                                                                                                                                         
-        let cityBiteDocuments = {
-
-
-            restoName: "CLIFF",
-            restoHours: "08:00 - 20:00",
-            restoAdress: "Rue De La Montagne 8, 1000 Brussels ",
-            foodType: "Brunch",
-            description: "CLIFF is a well-known brunch café in central Brussels, just steps from the Grand Place. It serves modern brunch dishes, quality specialty coffee, and creative drinks in a cozy, contemporary space that attracts both locals and visitors throughout the week."
-
-        }
+        app.get("/", (req, res) => {
+            res.send("jail!");
+        });
 
 
-        // Insert the documents into the specified collection        
-        const p = await col.insertOne(cityBiteDocuments);
+        app.get("/data", async (req, res) => {
+            try {
+                const data = await col.find({}).toArray();
+                console.log(data);
+                res.json(data);
+            } catch (error) {
+                console.error(error);
+                res.status(500).send("Can't read cityBite from database");
+            }
+        });
 
-        // Find the document
-
-        const document = await col.findOne();
-
-        // Print results
-        console.log(document);
-
+        app.listen(port, () => {
+            console.log(`App listening at http://localhost:${port}`);
+        });
     } catch (err) {
-        console.log(err.stack);
-    }
-
-    finally {
-        await client.close();
+        console.error("Failed to connect to MongoDB", err);
     }
 }
 
-run().catch(console.dir);
-
+startServer();
